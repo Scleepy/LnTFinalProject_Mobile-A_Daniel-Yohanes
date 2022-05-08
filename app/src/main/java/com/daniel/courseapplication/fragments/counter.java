8a33309aa942fa5489e97845cd494f6c85bf738e
Scleepy <danielyohanes03@gmail.com>
2022-05-09 02:11:01 +0700

    added counter + bug fixes

--- app/src/main/java/com/daniel/courseapplication/fragments/counter.java
+++ app/src/main/java/com/daniel/courseapplication/fragments/counter.java
@@ -1,5 +1,7 @@
 package com.daniel.courseapplication.fragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,12 +9,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.daniel.courseapplication.R;
 
 public class counter extends Fragment {
 
-    public counter(){
+    TextView counterText, counterTextName;
+
+    Button btnAdd, btnMin, btnReset;
+
+    SharedPreferences sPUser;
+
+    public counter() {
 
     }
 
@@ -20,6 +31,51 @@ public class counter extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_counter, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_counter, container, false);
+
+        counterText = view.findViewById(R.id.counterNumber);
+        counterTextName = view.findViewById(R.id.textNumberCounter);
+
+        sPUser = this.getActivity().getSharedPreferences("USERINFO", Context.MODE_PRIVATE);
+
+        String progressCounter = sPUser.getString("PROGRESSCOUNTER", "");
+
+        counterText.setText(progressCounter);
+
+        btnAdd = view.findViewById(R.id.btnAddition);
+        btnMin = view.findViewById(R.id.btnSubstraction);
+        btnReset = view.findViewById(R.id.btnReset);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int counterIndex = Integer.parseInt(counterText.getText().toString());
+                counterIndex++;
+                counterText.setText(Integer.toString(counterIndex));
+            }
+        });
+
+        btnMin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int counterIndex = Integer.parseInt(counterText.getText().toString());
+
+                if(counterIndex > 0){
+                    counterIndex--;
+                    counterText.setText(Integer.toString(counterIndex));
+                }
+            }
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counterText.setText("0");
+            }
+        });
+
+
+        return view;
     }
 }
