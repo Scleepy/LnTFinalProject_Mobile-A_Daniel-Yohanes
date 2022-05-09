4afbd9053dc86a947cb2b6640ddac695ca9e1545
Scleepy <danielyohanes03@gmail.com>
2022-05-09 17:06:54 +0700

    added triangle calculator

--- app/src/main/java/com/daniel/courseapplication/fragments/area.java
+++ app/src/main/java/com/daniel/courseapplication/fragments/area.java
@@ -12,13 +12,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.daniel.courseapplication.R;
 
 public class area extends Fragment {
 
-    EditText userfieldSquare;
-    TextView resultSquare, processTextSquare;
+    EditText userfieldSquare, editTextBaseTriangle, editTextHeightTriangle, resultTextTriangle, resultSquare;
+    TextView formulaTriangle, processTextSquare;
 
     public area(){
 
@@ -33,6 +34,12 @@ public class area extends Fragment {
         userfieldSquare = view.findViewById(R.id.editTextLengthSquare);
         resultSquare = view.findViewById(R.id.resultTextSquare);
         processTextSquare = view.findViewById(R.id.processTextSquare);
+
+        editTextBaseTriangle = view.findViewById(R.id.editTextBaseTriangle);
+        editTextHeightTriangle = view.findViewById(R.id.editTextHeightTriangle);
+        resultTextTriangle = view.findViewById(R.id.resultTextTriangle);
+        formulaTriangle = view.findViewById(R.id.formulaTriangle);
+
 
         userfieldSquare.addTextChangedListener(new TextWatcher() {
             @Override
@@ -60,8 +67,92 @@ public class area extends Fragment {
                 } else {
                     resultSquare.setText("Max 8 digits");
                 }
+            }
 
+            @Override
+            public void afterTextChanged(Editable editable) {
 
+            }
+        });
+
+        editTextBaseTriangle.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!editTextBaseTriangle.getText().toString().isEmpty() && !editTextHeightTriangle.getText().toString().isEmpty()){
+
+                    String base = editTextBaseTriangle.getText().toString();
+
+                    String height = editTextHeightTriangle.getText().toString();
+
+                    Double result = triangleArea(Double.parseDouble(base), Double.parseDouble(height));
+
+                    resultTextTriangle.setText(Double.toString(result));
+
+                    formulaTriangle.setText("Area =     x " + base + " x " + height);
+                } else if (editTextBaseTriangle.getText().toString().isEmpty() && editTextHeightTriangle.getText().toString().isEmpty()){
+                    resultTextTriangle.setText("Input values");
+                    formulaTriangle.setText("Area =     x ? x ?");
+                } else if (editTextBaseTriangle.getText().toString().isEmpty() && !editTextHeightTriangle.getText().toString().isEmpty()){
+                    resultTextTriangle.setText("Input base");
+
+                    String height = editTextHeightTriangle.getText().toString();
+
+                    formulaTriangle.setText("Area =     x ? x " + height);
+                } else if(!editTextBaseTriangle.getText().toString().isEmpty() && editTextHeightTriangle.getText().toString().isEmpty()){
+                    resultTextTriangle.setText("Input height");
+
+                    String base = editTextBaseTriangle.getText().toString();
+
+                    formulaTriangle.setText("Area =     x " + base + " x ?");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        editTextHeightTriangle.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!editTextBaseTriangle.getText().toString().isEmpty() && !editTextHeightTriangle.getText().toString().isEmpty()){
+
+                    String base = editTextBaseTriangle.getText().toString();
+
+                    String height = editTextHeightTriangle.getText().toString();
+
+                    Double result = triangleArea(Double.parseDouble(base), Double.parseDouble(height));
+
+                    resultTextTriangle.setText(Double.toString(result));
+
+                    formulaTriangle.setText("Area =     x " + base + " x " + height);
+                } else if (editTextBaseTriangle.getText().toString().isEmpty() && editTextHeightTriangle.getText().toString().isEmpty()){
+                    resultTextTriangle.setText("Input values");
+                    formulaTriangle.setText("Area =     x ? x ?");
+                } else if (editTextBaseTriangle.getText().toString().isEmpty() && !editTextHeightTriangle.getText().toString().isEmpty()){
+                    resultTextTriangle.setText("Input base");
+
+                    String height = editTextHeightTriangle.getText().toString();
+
+                    formulaTriangle.setText("Area =     x ? x " + height);
+                } else if(!editTextBaseTriangle.getText().toString().isEmpty() && editTextHeightTriangle.getText().toString().isEmpty()){
+                    resultTextTriangle.setText("Input height");
+
+                    String base = editTextBaseTriangle.getText().toString();
+
+                    formulaTriangle.setText("Area =     x " + base + " x ?");
+                }
             }
 
             @Override
@@ -78,7 +169,14 @@ public class area extends Fragment {
 
         double result = length * length;
 
+        return result;
+    }
+
+    public double triangleArea(Double base, Double height){
+
+        double result = (base * height)/2;
 
         return result;
     }
+
 }
